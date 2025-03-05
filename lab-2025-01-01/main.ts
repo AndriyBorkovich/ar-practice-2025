39c996fefa9899df37c2fe5bfff0821681262b1e
Andriy Borkovich <borkovich25andri@gmail.com>
2025-03-05 11:59:39 +0200

    completed lab 1

--- lab-2025-01-01/main.ts
+++ lab-2025-01-01/main.ts
@@ -1,136 +1,131 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const MODE = 'immersive-ar';
+const MODE = "immersive-ar";
 
 async function activateXR(): Promise<void> {
-    const canvas = document.createElement("canvas");
-    document.body.appendChild(canvas);
-    
-    const gl = canvas.getContext("webgl2", {xrCompatible: true});
-    if (!gl) throw new Error("WebGL not supported");
+  const canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
 
-    // FIX THIS:
-    const scene = null;
+  const gl = canvas.getContext("webgl2", { xrCompatible: true });
+  if (!gl) throw new Error("WebGL not supported");
 
-    // initialize materials
-    const materials = [
-        new THREE.MeshBasicMaterial({color: 0xff0000}),
-        new THREE.MeshBasicMaterial({color: 0x0000ff}),
-        new THREE.MeshBasicMaterial({color: 0x00ff00}),
-        new THREE.MeshBasicMaterial({color: 0xff00ff}),
-        new THREE.MeshBasicMaterial({color: 0x00ffff}),
-        new THREE.MeshBasicMaterial({color: 0xffff00})
-    ];
+  // Create a new THREE.Scene
+  const scene = new THREE.Scene();
 
-    const cube = new THREE.Mesh(new THREE.BoxGeometry(0.2, 0.2, 0.2), materials);
-    // set cube position
-    cube.position.set(1, 1, 1);
-    // add cube to scene
-    scene.add(cube);
+  // initialize materials
+  const materials = [
+    new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+    new THREE.MeshBasicMaterial({ color: 0xff00ff }),
+    new THREE.MeshBasicMaterial({ color: 0x00ffff }),
+    new THREE.MeshBasicMaterial({ color: 0xffff00 }),
+  ];
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
-    directionalLight.position.set(10, 15, 10);
-    scene.add(directionalLight);
-    
-    const renderer = new THREE.WebGLRenderer({
-        alpha: true,
-        preserveDrawingBuffer: true,
-        canvas: canvas,
-        context: gl
-    });
-    renderer.autoClear = false;
+  const cube = new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 1.5), materials);
+  // set cube position
+  cube.position.set(1, 1, 1);
+  // add cube to scene
+  scene.add(cube);
 
-    // FIX THIS:
-    const camera = null;
-    camera.matrixAutoUpdate = false;
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+  directionalLight.position.set(10, 15, 10);
+  scene.add(directionalLight);
 
-    if (!navigator.xr) {
-        throw new Error("WebXR is not supported by your browser");
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    preserveDrawingBuffer: true,
+    canvas: canvas,
+    context: gl,
+  });
+  renderer.autoClear = false;
+
+  // Create a new THREE.PerspectiveCamera
+  const camera = new THREE.PerspectiveCamera();
+  camera.matrixAutoUpdate = false;
+
+  if (!navigator.xr) {
+    throw new Error("WebXR is not supported by your browser");
+  }
+
+  try {
+    const supported = await navigator.xr.isSessionSupported(MODE);
+    if (!supported) {
+      throw new Error(`${MODE} mode is not supported by your browser/device`);
     }
+  } catch (e) {
+    throw new Error("Error checking WebXR support: " + e);
+  }
 
+  const session = await navigator.xr.requestSession(MODE, {
+    requiredFeatures: ["local"],
+  });
+
+  // Create a new XRWebGLLayer
+  const baseLayer = new XRWebGLLayer(session, gl);
+  session.updateRenderState({
+    baseLayer,
+  });
+
+  const referenceSpaceTypes: XRReferenceSpaceType[] = [
+    "local",
+    "local-floor",
+    "bounded-floor",
+    "unbounded",
+    "viewer",
+  ];
+
+  let referenceSpace: XRReferenceSpace | null = null;
+
+  // observe how reference space types and request reference space
+  // are applied to the scene
+  for (const spaceType of referenceSpaceTypes) {
     try {
-        const supported = await navigator.xr.isSessionSupported(MODE);
-        if (!supported) {
-            throw new Error(`${MODE} mode is not supported by your browser/device`);
-        }
+      // request reference space
+      referenceSpace = await session.requestReferenceSpace(spaceType);
+      console.log("Reference space established:", spaceType);
+      break;
     } catch (e) {
-        throw new Error('Error checking WebXR support: ' + e);
+      console.log(e);
+      console.log("Reference space failed:", spaceType);
+      continue;
     }
+  }
 
-    const session = await navigator.xr.requestSession(
-        MODE,
-        {
-            requiredFeatures: ['local']
-        }
-    );
+  if (!referenceSpace) {
+    throw new Error("No reference space could be established");
+  }
 
-    // FIX THIS:
-    const baseLayer = null;
-    session.updateRenderState({
-        baseLayer
-    });
-
-
-    const referenceSpaceTypes: XRReferenceSpaceType[] = [
-        'local',
-        'local-floor',
-        'bounded-floor',
-        'unbounded',
-        'viewer'
-    ];
-
-    let referenceSpace: XRReferenceSpace | null = null;
-
-    // observe how reference space types and request reference space
-    // are applied to the scene
-    for (const spaceType of referenceSpaceTypes) {
-        try {
-            // request reference space
-            // FIX THIS:
-            referenceSpace = null;
-            console.log('Reference space established:', spaceType);
-            break;
-        } catch(e) {
-            console.log(e);
-            console.log('Reference space failed:', spaceType);
-            continue;
-        }
-    }
-
-    if (!referenceSpace) {
-        throw new Error('No reference space could be established');
-    }
-
-    // Create a render loop that allows us to draw on the AR view.
-    const onXRFrame = (time: number, frame: XRFrame) => {
-        // Queue up the next draw request.
-        session.requestAnimationFrame(onXRFrame);
-    
-        const baseLayer = session.renderState.baseLayer;
-        if (!baseLayer) return;
-    
-        gl.bindFramebuffer(gl.FRAMEBUFFER, baseLayer.framebuffer)
-    
-        const pose = frame.getViewerPose(referenceSpace);
-        if (pose) {
-            const view = pose.views[0];
-    
-            const viewport = baseLayer.getViewport(view);
-            if (!viewport) return;
-            renderer.setSize(viewport.width, viewport.height)
-    
-            // Use the view's transform matrix and projection matrix to configure the THREE.camera.
-            camera.matrix.fromArray(view.transform.matrix)
-            camera.projectionMatrix.fromArray(view.projectionMatrix);
-            camera.updateMatrixWorld(true);
-    
-            // Render the scene with THREE.WebGLRenderer.
-            renderer.render(scene, camera)
-        }
-    }
-
+  // Create a render loop that allows us to draw on the AR view.
+  const onXRFrame = (time: number, frame: XRFrame) => {
+    // Queue up the next draw request.
     session.requestAnimationFrame(onXRFrame);
+
+    const baseLayer = session.renderState.baseLayer;
+    if (!baseLayer) return;
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, baseLayer.framebuffer);
+
+    const pose = frame.getViewerPose(referenceSpace);
+    if (pose) {
+      const view = pose.views[0];
+
+      const viewport = baseLayer.getViewport(view);
+      if (!viewport) return;
+      renderer.setSize(viewport.width, viewport.height);
+
+      // Use the view's transform matrix and projection matrix to configure the THREE.camera.
+      camera.matrix.fromArray(view.transform.matrix);
+      camera.projectionMatrix.fromArray(view.projectionMatrix);
+      camera.updateMatrixWorld(true);
+
+      // Render the scene with THREE.WebGLRenderer.
+      renderer.render(scene, camera);
+    }
+  };
+
+  session.requestAnimationFrame(onXRFrame);
 }
 
 // Make the function available globally
-(window as any).activateXR = activateXR; 
+(window as any).activateXR = activateXR;
